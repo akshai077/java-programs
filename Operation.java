4e16813561f87c1e40fcf315859eb3d8f2eb005b
akshai077 <akshaiku17@gmail.com>
2025-02-12 11:00:18 +0530

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,16 @@
+public class Operation{
+public static void main(String[]args){
+int x;
+x=3+4;
+System.out.println(x);
+int y;
+y=5-1;
+System.out.println(y);
+int z;
+z=5*2;
+System.out.println(z);
+int f;
+f=4/2;
+System.out.println(f);
+}
+}
---
+++ Var.java
@@ -0,0 +1,6 @@
+public class Var{
+public static void main(String[]args){
+String myMessage="hi";
+System.out.println(myMessage);
+}
+}
