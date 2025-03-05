ac6cc2b997530967b30e87d8d23a8942e06c2b7b
akshai077 <akshaiku17@gmail.com>
2025-03-05 10:13:27 +0530

    Add files via upload

---
+++ Complex.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+    public class Complex
+    {
+      int real;
+      int imaginary;
+      public Complex(int real, int imaginary)
+    {
+     this.real = real;
+     this.imaginary = imaginary;
+    }
+     public void display()
+   {
+     System.out.println(real + " + " + imaginary + "i");
+   }
+     public static void add(int r1, int i1, int r2, int i2)
+  {
+     int sumReal = r1 + r2;
+     int sumImaginary = i1 + i2;
+     System.out.println("After Addition = " + sumReal + " + " + sumImaginary + "i");
+  }
+     public static void main(String[] args)
+  {
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter the real part of the first complex number:");
+     int real1 = sc.nextInt();
+     System.out.println("Enter the imaginary part of the first complex number:");
+     int img1 = sc.nextInt();
+     System.out.println("Enter the real part of the second complex number:");
+     int real2 = sc.nextInt();
+     System.out.println("Enter the imaginary part of the second complex number:");
+     int img2 = sc.nextInt();
+     Complex first = new Complex(real1, img1);
+     Complex second = new Complex(real2, img2);
+     System.out.println("Complex Numbers are:");
+     first.display();
+     second.display();
+    add(first.real, first.imaginary, second.real, second.imaginary);
+  }
+}
---
+++ MatrixAddition.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+class Matrix
+  {
+    private int[][] matrix;
+    public Matrix(int rows, int columns)
+  {
+    matrix = new int[rows][columns];
+  }
+    public void inputMatrix(Scanner scanner)
+  {
+    for (int i = 0; i < matrix.length; i++)
+  {
+    for (int j = 0; j < matrix[0].length; j++)
+  {
+    matrix[i][j] = scanner.nextInt();
+  }
+  }
+  }
+   public Matrix add(Matrix other)
+  {
+   Matrix result = new Matrix(matrix.length, matrix[0].length);
+   for (int i = 0; i < matrix.length; i++)
+  {
+   for (int j = 0; j < matrix[0].length; j++)
+  {
+   result.matrix[i][j] = this.matrix[i][j] + other.matrix[i][j];
+  }
+  }
+  return result;
+  }
+  public void displayMatrix()
+  {
+  for (int i = 0; i < matrix.length; i++)
+  {
+  for (int j = 0; j < matrix[0].length; j++)
+  {
+  System.out.print(matrix[i][j] + "\t");
+  }
+  System.out.println();
+  }
+}
+}
+      public class MatrixAddition
+       {
+         public static void main(String[] args)
+       {
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Enter the number of rows: ");
+         int rows = scanner.nextInt();
+         System.out.print("Enter the number of columns: ");
+         int columns = scanner.nextInt();
+         Matrix matrix1 = new Matrix(rows, columns);
+         Matrix matrix2 = new Matrix(rows, columns);
+         System.out.println("Enter the elements of the first matrix:");
+         matrix1.inputMatrix(scanner);
+         System.out.println("Enter the elements of the second matrix:");
+         matrix2.inputMatrix(scanner);
+         Matrix sumMatrix = matrix1.add(matrix2);
+         System.out.println("Sum of the matrices:");
+         sumMatrix.displayMatrix();
+         scanner.close();
+      }
+}
